2f94ab6af1c1b7ac3306b2e4caa95189e633d23d
bwamie <bwamie@gmail.com>
2017-03-20 09:34:20 -0500

    Updated Inheritance on Owner and Person: Person and business using composition and Owner and Client using Inheritance

--- src/main/java/com/property/mgt/domain/Property.java
+++ src/main/java/com/property/mgt/domain/Property.java
@@ -1,12 +1,29 @@
 package com.property.mgt.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy=InheritanceType.JOINED)
 public class Property {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id")
 	private long propertyId;
+	
 	private String name;
+	private String description;
+	private String type;
 	private String address;
 	private String city;
 	private String state;
-	private String description;
+	private String status;
+
 
 }
